5eb7e8c96698b14b490f8277808c470091e75e6e
Castro <103647024+joaoguicastro@users.noreply.github.com>
2023-11-15 14:40:15 -0300

    finalizei o modulo 1

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,21 @@ import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database();
 
+
+//Route parameters, request body, query parameters
+
 export const routes = [
     {
         method: 'GET',
         path: buildRoutePath('/users'),
         handler:(req,res) => {
-            const users = database.select('users')
+            
+            const { search } = req.query
+
+            const users = database.select('users', search ? {
+                name: search,
+                email: search,
+            }: null)
 
             return res.end(JSON.stringify(users));
         }
@@ -36,8 +45,26 @@ export const routes = [
         method: 'DELETE',
         path: buildRoutePath('/users/:id'),
         handler: (req,res) => {
-            console.log(req.params)
-            return res.end()
+            const { id } = req.params;
+
+            database.delete('users', id)
+
+            return res.writeHead(204).end()
+        },
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (req,res) => {
+            const { id } = req.params;
+            const { name, email } = req.body
+
+            database.update('users', id, {
+                name,
+                email
+            })
+
+            return res.writeHead(204).end()
         },
     }
 
